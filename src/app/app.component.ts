84f71a66663960f3ce7fd5a21ee72767c27ec204
JavaHunk2020 <javahunk2020@gmail.com>
2023-09-06 06:04:31 +0530

    Two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'two-bindings';
+  username:string="";
+  password:string="";
+  email:string="";
+  mobile:string="";
+  address:string="";
+
+  psignup:any;
+
+  signup() {
+     
+   let signup={"jungle":this.username,'password':this.password,email:this.email,mobile:this.mobile,address:this.address};
+   console.log(signup);     
+   this.psignup=signup;
+  }
+  
 }
